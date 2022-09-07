844623b776418d8e5f18e0ee868447f70e363655
Simone Torrisi <7654979+storrisi@users.noreply.github.com>
2022-09-07 11:21:11 +0200

    added testVideoResults structure

---
+++ utils/testVideoResults.js
@@ -0,0 +1,93 @@
+const results = {
+    "b2b": {
+        "TRUE": "video1",
+        "FALSE": "video2"
+    },
+    "biggest_complain": {
+        "FUTURE": "",
+        "WHICH_LEARNING_PATH": "",
+        "TOO_OLD": "",
+        "WORK_QUICKLY": "",
+        "NOT_SURE_ABOUT_BOOTCAMPS": "",
+        "WHICH_COURSE": "",
+        "NO_PRACTICE": "",
+        "TOO_MUCH_MONEY": "",
+    },
+    "has_attended_courses": {
+        "TRUE": "",
+        "FALSE": "",
+    },
+    "is_developer_already": {
+        "TRUE": "",
+        "FALSE": "",
+    },
+    "is_currently_working": {
+        "TRUE": "",
+        "FALSE": "",
+    },
+    "type_of_courses": {
+        "VIDEO": "",
+        "OTHER": "",
+        "BOOTCAMP": "",
+    },
+    "knows_theory": {
+        "TRUE": "",
+        "FALSE": "",
+    },
+    "has_projects": {
+        "TRUE": "",
+        "FALSE": "",
+    },
+    "type_of_project": {
+        "SMALL_ALGORITHMS": "",
+        "WEBSITE": "",
+        "WEB_APPLICATION": "",
+        "BOT": "",
+        "OTHER": "",
+    },
+    "need_basics": {
+        "TRUE": "",
+        "FALSE": "",
+    },
+    "field_complexity": {
+        "HARD": "",
+        "MEDIUM": "",
+    },
+    "need_mentorship": {
+        "TRUE": "",
+        "FALSE": "",  
+    },
+    "motivation": {
+        "JOB":"",
+        "FREELANCE":"",
+        "PERSONAL_PROJECT":"",
+        "LEARNING":"",
+    },
+    "what_is_searching_for": {
+        "DIRECT_LESSONS":"",
+        "VIDEO_COURSES":"",
+        "DAILY_LESSONS":"",
+        "MENTORSHIP":"",
+        "TRY_PROGRAMMING":"",
+        "JOB_GUARANTEE":"",
+        "CERTIFICATION":"",
+        "LEARN_LANGUAGE":"",
+    },
+    "time_available": {
+        "TOO_LOW":"",
+        "NOT_ENOUGH":"",
+        "TOO_MUCH":"",
+        "OK":"",
+    },
+    "time_deadline": {
+        "1_3_MONTHS":"",
+        "3_6_MONTHS":"",
+        "MORE_6_MONTHS":"",
+    },
+    "need_degree" : {
+        "TRUE": "",
+        "FALSE": "", 
+    }
+}
+
+module.exports = results
